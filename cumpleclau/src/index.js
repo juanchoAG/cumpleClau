42dbb3b2cc0c3641532c99ca55cd977e114b1051
juanchoAG <juanlu1salbarra@gmail.com>
2025-08-15 19:07:32 +0200

    chore: GitHub Pages scripts y HashRouter para deploy

--- cumpleclau/src/index.js
+++ cumpleclau/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+// index.jsx / main.jsx
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
-);
+  </HashRouter>
+)
